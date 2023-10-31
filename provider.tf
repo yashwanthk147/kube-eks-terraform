e661fbd6698bee4bbe3da013478ff9ed9a36edbb
K YASHWANTH REDDY <yashwanthkotha147@gmail.com>
2023-10-31 17:49:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAZ4VXA6RJRXW7CNHQ"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
